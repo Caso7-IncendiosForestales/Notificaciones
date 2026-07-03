feb9f8686ecccd97c5d1c1daccf053dc03798ff9
Michael Escobar <mich.escobar@duocuc.cl>
2026-07-03 07:09:20 -0400

    Update NotificacionesApplication.java

--- src/main/java/com/conaf/notificaciones/NotificacionesApplication.java
+++ src/main/java/com/conaf/notificaciones/NotificacionesApplication.java
@@ -2,12 +2,20 @@ package com.conaf.notificaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "MS-10 Sistema de Notificaciones",
+        version = "1.0.0",
+        description = "Microservicio para el envío de alertas automáticas y mensajería en tiempo real a las brigadas y comunas afectadas - CONAF"
+    )
+)
 @SpringBootApplication
 public class NotificacionesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificacionesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NotificacionesApplication.class, args);
+    }
 }
